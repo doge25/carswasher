936ba93ee1feddb8a67f083087859362c905d303
mishanbl4 <rogmishanbl4@gmail.com>
2017-08-18 19:34:42 +0300

    logpas

--- Scripts/PHP/sayHello.php
+++ Scripts/PHP/sayHello.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>kek</title>
+	<meta charset="utf-8">
+	<title>иди в жопу</title>
 </head>
 <body>
 	<form>
 		<h1>Hello <?php echo $_REQUEST['Name']; ?></h1>
+		<?php
+			$Password = $_REQUEST['password'];
+			$Name = $_REQUEST['Name'];
+			if ($Password != '1234')
+			{
+				echo "error password not succesefull";
+			} 
+			else
+			{
+				echo "welcom from asshole";
+			}
+		?>
 	</form>
 </body>
 </html>
