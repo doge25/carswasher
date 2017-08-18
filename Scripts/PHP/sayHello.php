9977c4c404bc9e1430dd734c4b8b2f7dd99d94d6
mishanbl4 <rogmishanbl4@gmail.com>
2017-08-18 18:14:47 +0300

    asd
    kek

---
+++ Scripts/PHP/sayHello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kek</title>
+</head>
+<body>
+	<form>
+		<h1>Hello <?php echo $_REQUEST['Name']; ?></h1>
+	</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> ololo </title>
+</head>
+<body>
+	<center>
+		<table>
+				<tr>
+					<td>
+						email
+					</td>
+					<td>
+						<input type="text" name="">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						nickname
+					</td>
+					<td>
+						<input type="text" name="">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						password
+					</td>
+					<td>
+						<input type="text" name="">
+					</td>
+				</tr>
+		</table>
+	</center>
+	<form action="Scripts/PHP/sayHello.php" method="POST">
+		write your name fucking pidor
+		<input type="text" name="Name" id = "pidor">
+		<input type="" name="">
+	</form>
+
+</body>
+</html>
